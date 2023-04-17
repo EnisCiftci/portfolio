12508811a92130b326d0e067570557d9d4b01583
Boi <83432930+EnisCiftci@users.noreply.github.com>
2023-04-17 22:43:14 +0200

    +About Layout TODO: Responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
 import styles from "./about.css"
+import portrait from '../../assets/portrait.png'
 
 export default function About() {
     return (
         <section>
-            <div className={"test"}>
-                <div>
-                    <h3 className={"text-secondary"}>Hello, Im</h3>
-                    <h1>Enis Ciftci</h1>
+            <div className={"upper"}>
+                <div className={"greeting"}>
+                    <div>
+                        <h3 className={"text-secondary"}>Hello, I'm</h3>
+                        <h1>Enis Ciftci</h1>
+                    </div>
+                </div>
+                <div className={"portrait"}>
+                    <img src={portrait} alt={"ERR"}/>
                 </div>
             </div>
+            <div className={"lower"}>
+                <div className={"buttons"}>
+                    <a href={"#"} className={"btn btn-primary"}>Contact Me</a>
+                    <a href={"#"} className={"btn"}>Download CV</a>
+                </div>
+                <div className={"text"}>
+                    <p>
+                        <span className={"accent"}>Merhaba!</span> My name is Enis and I'm a 21-year-old
+                        student at Ruhr-Universität Bochum majoring in applied computer science.
+                        <br></br>
+                        My passion for coding started at a young age. I learned Java in school and fell in
+                        love with it right away, creating a range of projects for both school and for myself.
+                        <br></br>
+                        Throughout my time at university, I've sharpened my Java skills and have also gained
+                        experience in a variety of other programming languages and and developed and adopted solid coding
+                        ethics and practices. I'm always trying to further my knowledge and my skill set, and I'm
+                        interested in where my love of coding may lead me in the future.
+                        <br></br>
+                        Thank you for taking the time to visit my portfolio website, and I hope you enjoy exploring
+                        my projects and learning more about me.
+                    </p>
+                </div>
+            </div>
+            <div className={"skills"}>
+                <ul>
+                    <li><span className={"accent"}>></span>FullStack</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>Java</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>Swift</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>Kotlin</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>Angular</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>React</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>C#</li>
+                </ul>
+                <ul>
+                    <li><span className={"accent"}>></span>SQL</li>
+                </ul>
+            </div>
+
         </section>
     )
 }
